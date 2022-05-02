d47c098d0935e06b92d457c1a5ec5eb708e62226
xhlulu <xhlperso@gmail.com>
2022-05-02 00:27:51 -0400

    Refactor adding publication by ID

--- src/python/add_publication_by_id.py
+++ src/python/add_publication_by_id.py
@@ -8,52 +8,56 @@ from . import parse_issue_body
 from .add_publication import format_parsed_content, write_content_to_file
 
 
+def fetch_content(parsed):
 
-def fetch_content(di):
-    lab_members = yaml.safe_load(open("_data/authors.yml"))
-
-    method = di["method"]
-    identifier = di["identifier"]
-    month = di["month"]
-    day = di["day"]
+    method = parsed["method"]
+    identifier = parsed["identifier"]
         
     url = urlopen(
         f"https://api.semanticscholar.org/graph/v1/paper/{method}:{identifier}?fields=title,venue,year,authors.name,externalIds,url,abstract"
     )
     data = json.loads(url.read())
 
-    author_names = [di['name'] for di in data['authors']]
-    data['names'] = ", ".join(author_names).replace("\n", " ")
-    data['tags'] = data['venue']
-    data['venue'] = f"{data['venue']} {data['year']}"
-    data['shorthand'] = str(data['paperId'])
-    data['year'] = str(data['year'])
-    data['month'] = month
-    data['day'] = day
-    data['link'] = data['url']
+    return data
 
-    if 'ArXiv' in data['externalIds']:
-        data['link'] = f"https://arxiv.org/abs/{data['externalIds']['ArXiv']}"
-        data['shorthand'] = data['externalIds']['ArXiv']
+def wrangle_fetched_content(parsed, paper_json):
+    with open("_data/authors.yml") as f:
+        lab_members = yaml.safe_load(f)
+    
+    paper_json['month'] = parsed.get("month", "01")
+    paper_json['day'] = parsed.get("day", "01")
 
-    lab_members_names = {di['name'] for di in lab_members.values()}
-    lab_members_ids = {di['semantic_scholar_id'] for di in lab_members.values() if 'semantic_scholar_id' in di}
+    author_names = [parsed['name'] for parsed in paper_json['authors']]
+    paper_json['names'] = ", ".join(author_names).replace("\n", " ")
+    paper_json['tags'] = paper_json['venue']
+    paper_json['venue'] = f"{paper_json['venue']} {paper_json['year']}"
+    paper_json['shorthand'] = str(paper_json['paperId'])
+    paper_json['year'] = str(paper_json['year'])
+    paper_json['link'] = paper_json['url']
 
-    for author in data['authors']:
+    if 'ArXiv' in paper_json['externalIds']:
+        paper_json['link'] = f"https://arxiv.org/abs/{paper_json['externalIds']['ArXiv']}"
+        paper_json['shorthand'] = paper_json['externalIds']['ArXiv']
+
+    lab_members_names = {parsed['name'] for parsed in lab_members.values()}
+    lab_members_ids = {parsed['semantic_scholar_id'] for parsed in lab_members.values() if 'semantic_scholar_id' in parsed}
+
+    for author in paper_json['authors']:
         if author['authorId'] in lab_members_ids or author['name'] in lab_members_names:
-            data['author'] = author['name']
+            paper_json['author'] = author['name']
             break
 
-    del data['externalIds'], data['paperId'], data['url'], data['authors']
+    del paper_json['externalIds'], paper_json['paperId'], paper_json['url'], paper_json['authors']
 
-    return data
+    return paper_json
 
 if __name__ == "__main__":
     issue_body = os.environ['ISSUE_BODY']
 
     parsed = parse_issue_body(issue_body)
-    fetched = fetch_content(parsed)
-    formatted = format_parsed_content(fetched)
+    paper_json = fetch_content(parsed)
+    paper_json = wrangle_fetched_content(parsed, paper_json) # in-place
+    formatted = format_parsed_content(paper_json)
     write_content_to_file(formatted)
 
 
